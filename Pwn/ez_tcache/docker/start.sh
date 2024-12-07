c0f4e77611775500d740ab3c7db7575344aafac7
dbgbgtf1 <88665013@qq.com>
2024-12-07 17:45:10 +0800

    change env flag

--- Pwn/ez_tcache/docker/start.sh
+++ Pwn/ez_tcache/docker/start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-# Add your startup script
-if [ ! $DASFLAG ];then
- echo CBCTF{TEST_FLAG}|tee /home/ctf/flag
-else
- echo $DASFLAG|tee /home/ctf/flag 
-fi
+echo FLAG{TEST_FLAG}|tee /home/ctf/flag
 chown root:ctf /home/ctf/flag 
 chmod 740 /home/ctf/flag
 
-# DO NOT DELETE
 /etc/init.d/xinetd start;
 sleep infinity;
--- Pwn/ogw_orw/docker/start.sh
+++ Pwn/ogw_orw/docker/start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# Add your startup script
-echo CBCTF{TEST_FLAG}|tee /home/ctf/flag
+echo FLAG{TEST_FLAG}|tee /home/ctf/flag
 
 chown root:ctf /home/ctf/flag 
 chmod 740 /home/ctf/flag
 
 random_suffix=$(head -c 10 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | head -c 10)
 
-# 使用后缀重命名文件
 mv flag "flag${random_suffix}"
-# DO NOT DELETE
+
 /etc/init.d/xinetd start;
 sleep infinity;
